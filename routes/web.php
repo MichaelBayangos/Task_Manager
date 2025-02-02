bb61a794dc26b417732fe6387c82923f5639a9b6
MichaelBayangos <michaelbayangos34@gmail.com>
2025-02-02 11:32:17 +0800

    modified route controller for auth

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,15 @@ Route::get('/', function () {
 });
 
 // this is Route controller for AUTH
-Route::get('/Register',[AuthController::class,'showRegiter'])->name('show.register');
-Route::get('/Login',[AuthController::class,'showLogin'])->name('show.login');
-Route::post('/Register',[AuthController::class,'regiter'])->name('register');
-Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+Route::get('/Register',[AuthController::class,'showRegiter'])->name('show.register')->middleware('guest');
+Route::get('/Login',[AuthController::class,'showLogin'])->name('show.login')->middleware('guest');
+Route::post('/Register',[AuthController::class,'regiter'])->name('register')->middleware('guest');
+Route::post('/Login',[AuthController::class,'login'])->name('login')->middleware('guest');
+
+
 
 // logout routes
-Route::post('/Login',[AuthController::class,'login'])->name('login');
+Route::post('/logout',[AuthController::class,'logout'])->name('logout');
 
 
 // this is route resrouce for the CRUD operation of Task
